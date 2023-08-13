68300f6ac1eb157f265136cc04343e59e0f3a567
pankajdeh <pankajdehariya.seo@gmail.com>
2023-08-13 22:50:41 +0530

    change about section

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,12 +13,12 @@ const Home = () => {
         
         <div className=' max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'> 
             <div className=' flex flex-col justify-center h-full'>
-                <h2 className='text-4xl sm:text-7xl font-bold text-white mt-20' >I'm a Full Stack Developer...</h2>
+                <h2 className='text-4xl sm:text-7xl font-bold text-white mt-20' >I'm a MERN Stack Developer...</h2>
                 <p 
                 className='text-gray-500 py-4 max-w-md'>
-                    I have 1.5 years of experience building and designing website.
-                    Currently, I love to work on web application using technologies like
-                    React, Talwind, Next.js and GraphQL.
+                    I have 1 years of experience building and designing website.
+                    Currentl I love to work on web application using technologies like
+                    React.js, Talwind Css, node.js, mongodb, postman, etc.
 
                 </p>
                 <button 
